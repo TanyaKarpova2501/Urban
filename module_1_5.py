477aae00ab528627ebeb8fb22cad5c8bc8532072
TanyaKarpova2501 <Pochtasony2906@gmail.com>
2024-09-17 19:38:55 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = "chery", 1, 2, "true"
+print (immutable_var)
+#картеж не изменяется, он как хранилище данных, которые не нужно изменять, и для того чтоб не допустить ошибки используют его.
+mutable_lis = ["good ", "food ", 1, 2 ,"g"]
+print(mutable_lis)
+mutable_lis [0] = "module"
+print(mutable_lis)
