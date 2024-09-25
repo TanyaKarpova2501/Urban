2c0880e8ca6619ffaad634343c80b4322b3f84af
TanyaKarpova2501 <Pochtasony2906@gmail.com>
2024-09-25 13:17:19 +0700

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+while 1 > 0:
+    a = 0
+    my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+    n = int(input( my_list))
+    print(n )
+
+    if n % 2 == 0:
+        print('Верно')
+        continue
+    else :
+        print("Неверно")
+
+    break
