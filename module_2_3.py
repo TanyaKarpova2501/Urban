de2583c09a55ae57c4e938f34ec1d9ebbfd324f2
TanyaKarpova2501 <Pochtasony2906@gmail.com>
2024-09-27 20:54:42 +0700

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,10 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 num = 0
 while num < len(my_list):
-    for num in my_list:
-        if num >= 0:
-            print(num, end=",")
+        if my_list[num] >= 0:
+            print(my_list[num])
+            num += 1
+            continue
         else:
-            print(1 != 0)
+            print()
             break
