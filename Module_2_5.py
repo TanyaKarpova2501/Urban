b61e6d5f88164a2fbc42311fe6423a83109368d9
TanyaKarpova2501 <Pochtasony2906@gmail.com>
2024-10-04 21:07:51 +0700

    Add files via upload

---
+++ Module_2_5.py
@@ -0,0 +1,17 @@
+from pydoc import apropos
+
+
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    print(matrix)
+
+result_1 = get_matrix( 5, 7, 15)
+result_2 = get_matrix( 8, 3, 10)
+result_3 = get_matrix( 2, 5, 20)
+print(result_1)
+print(result_2)
+print(result_3)
