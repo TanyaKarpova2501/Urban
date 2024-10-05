7c7a0cf52c6d58cc2e6134c2a6908c3f8e3c5071
TanyaKarpova2501 <Pochtasony2906@gmail.com>
2024-10-05 20:09:46 +0700

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -1,6 +1,7 @@
-n = int(input("введите число "))
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13 ,14, 15]
+n = int(input("Введите от 1 до 15 "))
 p = 2
-for p in range(2, (n+1)):
+for p in range(len(number)):
     for i in range(2, p):
         if p % i == 0:
             break
