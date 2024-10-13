bc0faee2a25a8fee26d700ff5019521ae417ca37
TanyaKarpova2501 <Pochtasony2906@gmail.com>
2024-10-13 20:53:13 +0700

    Add files via upload

--- Module_3_2.py
+++ Module_3_2.py
@@ -1,20 +1,21 @@
+from main import check
+
+
 def send_email( message, recepient, sender = "university.help@gmail.com"):
-    print(message, recepient, sender)
+    import re
 
-    def send_email(email):
-        verify = send_email(email)
-        if verify == recepient:
-            print(f'{email} is a valid email address')
-        recepient = (r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$')
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
-
+    def check(email):
+        if (re.send_email(regex, email)):
+            print("Valid Email")
+        else:
+            print("Invalid Email")
 
 my_email = input('Введите адрес почты: ')
 
-send_email('Невозможно отравить письмо на ', recepient= 'pochtasony@gmail c',
-           sender = "university.help@gmail.com")
+
+send_email('Невозможно отравить письмо на ',recepient = my_email, sender="university.help@gmail.com")
 send_email('Нельзя отправить сообщение самому себе ', recepient="university.help@gmail.com",
-           sender="university.help@gmail.com")
-send_email('Не стандартный отправитель. Письмо отправлено с ', sender="university.help@gmail.com",
-           recepient = 'на poctasony2906@gmail.uk')
-send_email('Письмо успешно отправлено ', 'pochtasony2906@gmail.com, от',)
+                 sender="university.help@gmail.com")
+send_email('Письмо успешно отправлено ', "от ")
