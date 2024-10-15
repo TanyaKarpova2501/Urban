c5a5193fd654e2004030f773630fc5750e8ff6f7
TanyaKarpova2501 <Pochtasony2906@gmail.com>
2024-10-15 20:50:13 +0700

    Add files via upload

--- dop_module_1.py
+++ dop_module_1.py
@@ -7,4 +7,6 @@ grades_n = [sum(grades[0])/len(grades[0]),
             sum(grades[3])/len(grades[3]),
             sum(grades[4])/len(grades[4])]
 
+
+print(sorted(students))
 print(dict(zip(students, grades_n)))
